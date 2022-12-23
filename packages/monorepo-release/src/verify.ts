8954a9b30fcd9f7a13e350eefaaae400e98b0046
Balázs Orbán <info@balazsorban.com>
2022-12-23 06:00:44 +0100

    fix: only check for `NPM_TOKEN`

--- packages/monorepo-release/src/verify.ts
+++ packages/monorepo-release/src/verify.ts
@@ -1,9 +1,6 @@
 export async function verify() {
-	if (!process.env.NPM_TOKEN_PKG) {
-		throw new Error("NPM_TOKEN_PKG is not set")
-	}
-	if (!process.env.NPM_TOKEN_ORG) {
-		throw new Error("NPM_TOKEN_ORG is not set")
+	if (!process.env.NPM_TOKEN) {
+		throw new Error("NPM_TOKEN is not set")
 	}
 	if (!process.env.RELEASE_TOKEN) {
 		throw new Error("RELEASE_TOKEN is not set")
