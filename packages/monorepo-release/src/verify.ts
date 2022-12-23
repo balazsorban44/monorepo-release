27e095660842e949cc634b4fd145ff5a3d031c5c
Balázs Orbán <info@balazsorban.com>
2022-12-23 06:01:12 +0100

    fix: renamte `RELEASE_TOKEN` to `GITHUB_TOKEN`

--- packages/monorepo-release/src/verify.ts
+++ packages/monorepo-release/src/verify.ts
@@ -2,7 +2,7 @@ export async function verify() {
 	if (!process.env.NPM_TOKEN) {
 		throw new Error("NPM_TOKEN is not set")
 	}
-	if (!process.env.RELEASE_TOKEN) {
-		throw new Error("RELEASE_TOKEN is not set")
+	if (!process.env.GITHUB_TOKEN) {
+		throw new Error("GITHUB_TOKEN is not set")
 	}
 }
