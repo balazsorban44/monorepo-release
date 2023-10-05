615f798236465d121ac6df9a83ae550a7ff6e928
Balázs Orbán <info@balazsorban.com>
2023-10-05 16:39:00 +0200

    fix: skip verifying tokens with `--peek`

--- packages/monorepo-release/src/config.ts
+++ packages/monorepo-release/src/config.ts
@@ -13,17 +13,19 @@ export interface Config {
 
 const json = await pkgJson.read("./")
 
+const peek = !!process.env.PEEK || process.argv.includes("--peek")
 export const defaultConfig: Config = {
 	releaseBranches: ["main"],
 	BREAKING_COMMIT_MSG: "BREAKING CHANGE:",
 	RELEASE_COMMIT_MSG: "chore(release): bump package version(s) [skip ci]",
 	RELEASE_COMMIT_TYPES: ["feat", "fix"],
-	noVerify: !!process.env.NO_VERIFY || process.argv.includes("--no-verify"),
+	noVerify:
+		!!process.env.NO_VERIFY || process.argv.includes("--no-verify") || peek,
 	dryRun:
 		!process.env.CI ||
 		!!process.env.DRY_RUN ||
 		process.argv.includes("--dry-run"),
 	verbose: !!process.env.VERBOSE || process.argv.includes("--verbose"),
-	peek: !!process.env.PEEK || process.argv.includes("--peek"),
+	peek,
 	...json.release,
 }
